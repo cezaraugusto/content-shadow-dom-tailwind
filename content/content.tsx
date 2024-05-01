b68f95e29e35ce47fe8ddd7dd04fed20ce8f5c0a
Cezar Augusto <boss@cezaraugusto.net>
2024-05-01 18:05:19 -0300

    Add shadow dom w/ content_scripts example

---
+++ content/content.tsx
@@ -0,0 +1,61 @@
+import ReactDOM from "react-dom/client";
+import ContentApp from "./ContentApp";
+import("./base.css");
+import("./content.css");
+
+setTimeout(initial, 1000);
+
+const TAILWIND_URL =
+  "https://cdn.jsdelivr.net/npm/tailwindcss@^2.0/dist/tailwind.min.css";
+
+const appendTailwindStyleData = (shadowRoot: ShadowRoot) => {
+// Load and inject the local Tailwind CSS file into the shadow DOM
+  const styleSheet = document.createElement("style");
+  // Change this path to your actual Tailwind CSS file's location
+  fetch(TAILWIND_URL)
+    .then((response) => response.text())
+    .then((css) => {
+      styleSheet.textContent = css;
+      shadowRoot.appendChild(styleSheet);
+    });
+  }
+
+  const appendTailwindStyleLink = (shadowRoot: ShadowRoot) => {
+    // Import Tailwind CSS and inject it into the shadow DOM
+    const styleSheet = document.createElement("link");
+    styleSheet.rel = "stylesheet";
+    styleSheet.href = TAILWIND_URL;
+    shadowRoot.appendChild(styleSheet);
+  }
+
+
+function initial() {
+  // Create a new div element to host the shadow root.
+  // Styles for this div is in `content/content.css`
+  const hostDiv = document.createElement("div");
+  hostDiv.id = "extension-host";
+  document.body.appendChild(hostDiv);
+
+  // Attach the shadow DOM to the hostDiv and set the mode to
+  // 'open' for accessibility from JavaScript.
+  // Ref https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
+  const shadowRoot = hostDiv.attachShadow({ mode: "open" });
+
+  // Create a new div element that will be the root container for the React app
+  const rootDiv = document.createElement("div");
+  rootDiv.id = "extension-root";
+  shadowRoot.appendChild(rootDiv);
+
+  // You can use either `appendTailwindStyleData` or `appendTailwindStyleLink`
+  // to inject Tailwind CSS into the shadow DOM.
+
+  // Option 1: Inject Tailwind CSS using a <style> tag
+  // appendTailwindStyleLink(shadowRoot);
+
+  // Option 2: Inject Tailwind CSS using a <link> tag
+  appendTailwindStyleData(shadowRoot);
+
+  // Use `createRoot` to create a root, then render the <ContentApp /> component
+  const root = ReactDOM.createRoot(rootDiv);
+  root.render(<ContentApp />);
+}
